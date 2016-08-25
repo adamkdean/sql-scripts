38b7d901678b4265678f87f7623b7f03111a1198
Adam K Dean <adamkdean@googlemail.com>
2016-08-25 23:11:24 +0100

    Update and rename list-new-accounts.md to list-new-accounts.sql

--- scripts/list-new-accounts.sql
+++ scripts/list-new-accounts.sql
@@ -1,7 +1,5 @@
-```sql
 SELECT `character`.Name, `character`.LastName, account.ID, account.IsActive
 FROM account
 INNER JOIN `character` ON account.ID = `character`.AccountID
 ORDER BY Id
 DESC LIMIT 30
-```
